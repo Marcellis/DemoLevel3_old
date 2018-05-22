10fd5f7d7e15b8712d394f8b08b8985a4dd0f6ae
Marcellis <m.m.c.m.marcellis@hva.nl>
2018-05-22 20:09:08 +0200

    adjustments

--- app/src/main/java/com/example/marmm/demolevel3/MainActivity.java
+++ app/src/main/java/com/example/marmm/demolevel3/MainActivity.java
@@ -17,9 +17,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
